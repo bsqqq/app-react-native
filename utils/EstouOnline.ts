8319a3b6a419cec8f57060984415a09932151608
Vinicius Moura <viniciusmouraaragao@hotmail.com>
2021-08-16 11:24:53 -0300

    fix: alert quando os dados necessários forem baixados para a aplicação

--- utils/EstouOnline.ts
+++ utils/EstouOnline.ts
@@ -1,12 +1,14 @@
 import netinfo from '@react-native-community/netinfo'
 import atualizacoes from './TenhoAtualizacoes'
 
-export function estouOnline(): boolean {
+export async function estouOnline(): Promise<boolean> {
     var conectado: boolean = false
-    netinfo.addEventListener(state => {
+    netinfo.addEventListener(async state => {
         conectado = Boolean(state.isConnected)
         if(conectado == true) {
-            atualizacoes()
+            await atualizacoes()
+            .then(() => alert('Todos os dados foram baixados com sucesso.'))
+            .catch(() => alert('Erro: os dados não foram baixados corretamente, tente novamente saindo e entrando novamente no app.'))
         }
     })
     return conectado
